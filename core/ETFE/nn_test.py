8c21c10cb0f2011262b17212dff3ccee4dda8870
kmolLin <pyquino@gmail.com>
2018-11-13 19:47:56 +0800

    test for nnwork curve fitting

--- core/ETFE/nn_test.py
+++ core/ETFE/nn_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation
 import matplotlib.pyplot as plt
 from etfe import ETFE, sys_frq_rep
@@ -27,28 +27,34 @@ if __name__ == '__main__':
     mag, pha = sys_frq_rep(0.01, real_value, imag_value, tfreq,
                            tmag_sys, tphase)
     mag[mag == -inf] = 0
+
     x_train = np.array(tfreq_h)
     y_train = np.array(mag)
 
     x_test = np.array(tfreq_h)
     y_test = np.array(mag)
 
-    model = Sequential()
-    model.add(Dense(units=100, input_dim=1, kernel_initializer='normal'))
-    model.add(Activation('relu'))
-    model.add(Dense(units=450))
-    model.add(Activation('relu'))
-    model.add(Dense(units=1))
+    changmode = True
+    if changmode:
+        model = load_model("magnitude.h5")
+        classes = model.predict(x_test, batch_size=1)
+    else:
+        model = Sequential()
+        model.add(Dense(units=100, input_dim=1, kernel_initializer='normal'))
+        model.add(Activation('relu'))
+        model.add(Dense(units=450))
+        model.add(Activation('relu'))
+        model.add(Dense(units=1))
 
-    model.compile(loss='mean_squared_error',
-                  optimizer='adam')
+        model.compile(loss='mean_squared_error',
+                      optimizer='adam')
 
-    model.fit(x_train, y_train, epochs=100, batch_size=50, verbose=1)
+        model.fit(x_train, y_train, epochs=100, batch_size=50, verbose=1)
 
-    loss_and_metrics = model.evaluate(x_test, y_test, batch_size=100)
+        loss_and_metrics = model.evaluate(x_test, y_test, batch_size=100)
 
-    classes = model.predict(x_test, batch_size=1)
-    model.save('magnitude.h5')
+        classes = model.predict(x_test, batch_size=1)
+        model.save('magnitude.h5')
 
     # TODO : add method to save mode with phase
 
@@ -56,29 +62,33 @@ if __name__ == '__main__':
     y_train = np.array(pha)
 
     x_test = np.array(tfreq_h)
-    y_test = np.array(pha)
+    y_test1 = np.array(pha)
 
-    model_phase = Sequential()
-    model_phase.add(Dense(units=100, input_dim=1, kernel_initializer='normal'))
-    model_phase.add(Activation('relu'))
-    model_phase.add(Dense(units=450))
-    model_phase.add(Activation('relu'))
-    model_phase.add(Dense(units=1))
+    if changmode:
+        model_phase = load_model("phase.h5")
+        classes_phase = model_phase.predict(x_test, batch_size=1)
+    else:
+        model_phase = Sequential()
+        model_phase.add(Dense(units=100, input_dim=1, kernel_initializer='normal'))
+        model_phase.add(Activation('relu'))
+        model_phase.add(Dense(units=450))
+        model_phase.add(Activation('relu'))
+        model_phase.add(Dense(units=1))
 
-    model_phase.compile(loss='mean_squared_error',
-                  optimizer='adam')
+        model_phase.compile(loss='mean_squared_error',
+                      optimizer='adam')
 
-    model_phase.fit(x_train, y_train, epochs=100, batch_size=50, verbose=1)
+        model_phase.fit(x_train, y_train, epochs=100, batch_size=50, verbose=1)
 
-    loss_and_metrics_phase = model_phase.evaluate(x_test, y_test, batch_size=100)
+        loss_and_metrics_phase = model_phase.evaluate(x_test, y_test1, batch_size=100)
 
-    classes_phase = model_phase.predict(x_test, batch_size=1)
-    model.save('phase.h5')
+        classes_phase = model_phase.predict(x_test, batch_size=1)
+        model.save('phase.h5')
 
     test = x_test.reshape(-1)
     tmp_container = [mag, classes]
     tmp_phase = [pha, classes_phase]
     plt.plot(test, classes, c='r')
     plt.plot(test, y_test, c='b')
-    bode_plot(test, tmp_container, tmp_phase, True)
+    #bode_plot(test, tmp_container, tmp_phase, True)
     plt.show()
