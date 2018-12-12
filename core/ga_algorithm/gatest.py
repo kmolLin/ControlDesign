ac120f89d053ea29e844a8c828d21ee18d24e82a
KmolLin <smpss91341@gmail.com>
2018-12-13 00:46:14 +0800

    update ga program

--- core/ga_algorithm/gatest.py
+++ core/ga_algorithm/gatest.py
@@ -7,17 +7,41 @@ from etfe import ETFE, sys_frq_rep
 from scipy.signal import lti, bode
 import numpy as np
 from bodeplot_module import bode_plot
+from scipy import signal as sg
 import matplotlib.pyplot as plt
 
 #block = DialogBlock([1], [1, 1, 2], )
-upper = [1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000]
-lower = [-1000.0, -1000.0, -1000.0, -1000.0, -1000.0, -1000.0, -1000.0, -1000.0]
+upper = [100.0, 100.0]
+lower = [-100.0, -100.0]
 
 
-def test_algorithm_rga(tfreq_h, mag):
+def calcc2d(e, num, den, sampletime):
+    u = []
+    for k, e_k in enumerate(e):
+        sum1 = 0
+        for i, num_i in enumerate(num):
+            if k - i < 0:
+                continue
+            else:
+                sum1 += num[i] * e[k - i]
+        sum2 = 0
+        for io in range(1, len(den)):
+            if k == 0:
+                sum2 += 0
+                continue
+            if k - io < 0:
+                sum2 += 0
+            else:
+                sum2 += den[io] * u[k - io]
+        u.append(sum1 - sum2)
+    t = np.arange(0, sampletime * (len(e)), sampletime)
+    return t, u
+
+
+def test_algorithm_rga(data_time_step, u_input_data, y_output_data):
     """Real-coded genetic algorithm."""
-    fun1 = Genetic(Fitne(tfreq_h, mag, upper, lower), {
-        'maxGen': 500, 'report': 10,
+    fun1 = Genetic(Fitne(data_time_step, u_input_data, y_output_data, upper, lower), {
+        'maxGen': 100, 'report': 10,
         # Genetic
         'nPop': 100,
         'pCross': 0.95,
@@ -30,34 +54,48 @@ def test_algorithm_rga(tfreq_h, mag):
 
 
 if __name__ == "__main__":
-    file = open("../ETFE/testcode.txt", "r")
-    lines = file.readlines()
-    timex = []
-    input = []
-    output = []
-    for line in lines:
-        timex.append(float(line.split(' ')[0]))
-        input.append(float(line.split(' ')[1]))
-        output.append(float(line.split(' ')[2]))
-    # TODO : N = 8 * 1024 ???
-    n = 1024. * 16
-    # t_freq_h: units (Hz)
-    # t_mag: units (DB)
-    # t_phase: units (Deg)
-    tfreq, tfreq_h, tmag_sys, tphase, imag_value, real_value = \
-        ETFE(input, 0.0005, n, output)
-    mag, pha = sys_frq_rep(0.01, real_value, imag_value, tfreq,
-                           tmag_sys, tphase)
+    # file = open("../ETFE/testcode.txt", "r")
+    # lines = file.readlines()
+    # timex = []
+    # input = []
+    # output = []
+    # for line in lines:
+    #     timex.append(float(line.split(' ')[0]))
+    #     input.append(float(line.split(' ')[1]))
+    #     output.append(float(line.split(' ')[2]))
+    # # TODO : N = 8 * 1024 ???
+    # n = 1024. * 16
+    # # t_freq_h: units (Hz)
+    # # t_mag: units (DB)
+    # # t_phase: units (Deg)
+    # tfreq, tfreq_h, tmag_sys, tphase, imag_value, real_value = \
+    #     ETFE(input, 0.0005, n, output)
+    # mag, pha = sys_frq_rep(0.01, real_value, imag_value, tfreq,
+    #                        tmag_sys, tphase)
 
     # print(tfreq_h[:10])
     # print(len(tfreq_h))
     # print(len(mag))
-
-    a, b = test_algorithm_rga(tfreq_h, mag)
+    num = [10]
+    den = [1, 10]
+    tf = sg.TransferFunction(num, den)
+    t, yout = sg.step(tf)
+    a = np.ones(len(t))
+    a, b = test_algorithm_rga(len(t), a, yout)
     print(a)
-    system = lti([a[0], a[1], a[2]], [a[3], a[4], a[5], a[6], a[7]])
-    w, mag, phase = bode(system, w=np.array(tfreq_h))
-    bode_plot(w, mag, phase, True)
+    #
+    # dd, d1, d3d = sg.cont2discrete((num, den), 0.002, method="bilinear")
+    # t, u = calcc2d(a.tolist(), dd[0], d1, d3d)
+    # print(type(yout))
+    # plt.plot(t, u, 'r')
+    # plt.plot(t, u, 'g')
+    # plt.show()
+
+    # a, b = test_algorithm_rga(tfreq_h, mag)
+    # print(a)
+    # system = lti([a[0], a[1], a[2]], [a[3], a[4], a[5], a[6], a[7]])
+    # w, mag, phase = bode(system, w=np.array(tfreq_h))
+    # bode_plot(w, mag, phase, True)
     # fig, ax = plt.subplots()
     # ax.plot(T, yout)
     # plt.show()
--- core/ga_algorithm/src/fitnessfunc.pyx
+++ core/ga_algorithm/src/fitnessfunc.pyx
@@ -5,28 +5,57 @@ from scipy.interpolate import interp1d
 from scipy.signal import bode, lti
 import numpy as np
 cimport numpy as np
+from scipy.signal import step, TransferFunction, cont2discrete
 from numpy cimport float64_t, ndarray
 
 
 cdef class Fitne(Verification):
 
-    cdef ndarray data_mag, freqdata, lower, upper
+    cdef ndarray , u_input_data, lower, upper, y_output_data
     cdef int data_len
+    cdef int data_time_step
     
-    def __cinit__(self, freq_data:list, data_of_mag:np.ndarray, limitup:list, limitlow:list):
+    def __cinit__(self, data_time_step:int, u_input_data:np.ndarray, y_output_data:np.ndarray,
+                  limitup:list, limitlow:list):
         # kp/(1,1,2+kp)
         # self.block = block
-        self.data_mag = data_of_mag
+        self.data_time_step = data_time_step
         self.upper = np.array(limitup)
         self.lower = np.array(limitlow)
-        self.freqdata = np.array(freq_data)
-        self.data_len = len(self.freqdata)
+        self.u_input_data = np.array(u_input_data)
+        # self.data_len = len(self.freqdata)
+        self.y_output_data = y_output_data
         
     def __call__(self, ndarray[float64_t, ndim=1] v):
         return self.run(v)
-        
+
+
+    cdef list calcc2d(self, e:np.ndarray, num:np.ndarray, den:np.ndarray ,sampletime:float):
+        cdef list u = []
+        cdef int k, i, io
+        cdef double sum1, sum2
+        for k, e_k in enumerate(e):
+            sum1 = 0
+
+            for i, num_i in enumerate(num):
+                if k - i < 0:
+                    continue
+                else:
+                    sum1 += num[i] * e[k - i]
+            sum2 = 0
+            for io in range(1, len(den)):
+                if k == 0:
+                    sum2 += 0
+                    continue
+                if k - io < 0:
+                    sum2 += 0
+                else:
+                    sum2 += den[io] * u[k - io]
+            u.append(sum1 - sum2)
+        return u
+
     cdef int get_nParm(self):
-        return 8
+        return 2
     
     cdef ndarray[float64_t, ndim=1] get_upper(self):
         return self.upper
@@ -35,12 +64,22 @@ cdef class Fitne(Verification):
         return self.lower
     
     cdef double run(self, np.ndarray v):
+        cdef np.ndarray dd, d1, a
+        cdef float d3d
+        cdef list u = []
+        u = []
 
-        if v[0] == 0 or v[3] == 0:
+        if v[0] == 0:
             return 999999999
-        system = lti([v[0], v[1], v[2]], [v[3], v[4], v[5], v[6], v[7]])
-        w, mag, phase = bode(system, w=self.freqdata, n=self.data_len)
-        return sum(np.hypot(self.data_mag, mag))
+        # num =
+        # den =
+        # tf = TransferFunction([v[0]], [1, v[1]])
+        # t, yout = step(tf)
+        a = np.ones(self.data_time_step)
+
+        dd, d1, d3d = cont2discrete(([v[0]], [1, v[1]]), 0.002, method="bilinear")
+        u = self.calcc2d(a, dd[0], d1, d3d)
+        return sum(np.hypot(self.y_output_data, np.array(u)))
     
     cpdef object get_result(self, ndarray[float64_t, ndim=1] v):
         return v
